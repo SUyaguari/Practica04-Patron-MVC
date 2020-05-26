b6eaffc6760083f3787b1e58446bcb41cb07a3d0
Estudiantes <Estudiantes@192.168.1.6>
2020-05-26 12:32:10 -0500

    Clase interfaz ITelefonoDAO, cntiene los metodos CRUD

---
+++ src/ec/edu/ups/idao/ITelefonoDAO.java
@@ -0,0 +1,12 @@
+package ec.edu.ups.idao;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public interface ITelefonoDAO {
+    public void create();
+    public void read();
+    public void update();
+    public void delite();
+}
