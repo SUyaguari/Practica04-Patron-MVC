fd17fd4325b285ba6f00c92b17c3de342fbdab2c
Estudiantes <Estudiantes@192.168.1.6>
2020-05-26 12:42:40 -0500

    Clase interfaz IUsuarioDAO, cntiene los metodos CRUD

---
+++ src/ec/edu/ups/idao/IUsuarioDAO.java
@@ -0,0 +1,12 @@
+package ec.edu.ups.idao;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public interface IUsuarioDAO {
+    public void create();
+    public void read();
+    public void update();
+    public void delite();
+}
