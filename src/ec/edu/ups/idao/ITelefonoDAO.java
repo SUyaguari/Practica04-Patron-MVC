41adada24ed77c4b28c5efc75a72a01d723d335e
Estudiantes <Estudiantes@192.168.1.6>
2020-05-26 12:38:38 -0500

    Actualizacion de parametros de la clase ITelefonoDAO

--- src/ec/edu/ups/idao/ITelefonoDAO.java
+++ src/ec/edu/ups/idao/ITelefonoDAO.java
@@ -1,12 +1,16 @@
 package ec.edu.ups.idao;
 
+import ec.edu.ups.modelo.Telefono;
+import java.util.Collection;
+
 /**
  *
  * @author Estudiantes
  */
 public interface ITelefonoDAO {
-    public void create();
-    public void read();
-    public void update();
-    public void delite();
+    public Collection<Telefono> findAll();
+    public void create(Telefono telefono);
+    public Telefono read(String codigo);
+    public void update(Telefono telefono);
+    public void delite(Telefono telefono);
 }
