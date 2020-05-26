ee91a430cd7e2c13d8f4ef26673ce923f4cb8b94
Estudiantes <Estudiantes@192.168.1.6>
2020-05-26 15:54:58 -0500

    Clase ControladorTelefono

---
+++ src/ec/edu/ups/controlador/ControladorTelefono.java
@@ -0,0 +1,52 @@
+package ec.edu.ups.controlador;
+
+import ec.edu.ups.dao.TelefonoDAO;
+import ec.edu.ups.idao.ITelefonoDAO;
+import ec.edu.ups.modelo.Telefono;
+import ec.edu.ups.vista.VistaTelefono;
+import java.util.Collection;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class ControladorTelefono {
+    private VistaTelefono vistaTelefono;
+    private Telefono telefono;
+    private ITelefonoDAO telefonoDAO;
+
+    public ControladorTelefono() {
+    }
+
+    public ControladorTelefono(VistaTelefono vistaTelefono, TelefonoDAO telefonoDAO) {
+        this.vistaTelefono = vistaTelefono;
+        this.telefonoDAO = telefonoDAO;
+    }
+    
+    public void telefonoCrear(){
+        telefono = vistaTelefono.registrar();
+        telefonoDAO.create(telefono);
+    }
+    
+    public void mostrarTelefono(){
+        String codigo = vistaTelefono.buscarTelefono();
+        telefono = telefonoDAO.read(codigo);
+        vistaTelefono.mostrarTelefono(telefono);
+    }
+    
+    public void modificarTelefono(){
+        telefono = vistaTelefono.modificar();
+        telefonoDAO.update(telefono);
+    }
+    
+    public void verTelefonos(){
+        Collection<Telefono> telefonos;
+        telefonos = telefonoDAO.findAll();
+        if(!telefonos.isEmpty()){
+            vistaTelefono.mostrarTelefonos(telefonos);
+        }else{
+            System.out.println("Agenda Vacia");
+        }
+    }
+    
+}
